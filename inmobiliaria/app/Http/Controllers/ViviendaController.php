d15d35ceaa3a64697c4361be0283875fc6f4ed48
Raúl <ruizvela271@gmail.com>
2020-05-19 11:44:11 +0200

    Created model: Vivienda

---
+++ inmobiliaria/app/Http/Controllers/ViviendaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\vivienda;
+use Illuminate\Http\Request;
+
+class ViviendaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\vivienda  $vivienda
+     * @return \Illuminate\Http\Response
+     */
+    public function show(vivienda $vivienda)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\vivienda  $vivienda
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(vivienda $vivienda)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\vivienda  $vivienda
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, vivienda $vivienda)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\vivienda  $vivienda
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(vivienda $vivienda)
+    {
+        //
+    }
+}
---
+++ inmobiliaria/database/factories/ViviendaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\vivienda;
+use Faker\Generator as Faker;
+
+$factory->define(vivienda::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
