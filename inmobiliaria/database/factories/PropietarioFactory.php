fd56325a37a42824c27245a0e9a15e5c5ede0a34
Raúl <ruizvela271@gmail.com>
2020-05-19 12:04:14 +0200

    Created propietario model

---
+++ inmobiliaria/database/factories/PropietarioFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\propietario;
+use Faker\Generator as Faker;
+
+$factory->define(propietario::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
